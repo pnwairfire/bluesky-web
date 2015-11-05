77546adc3a7ff5b92c63c2eb1abdcb3b464ff394
Joel Dubowy <jdubowy@gmail.com>
2015-11-05 06:46:04 -0800

    Add package version

--- blueskyweb/__init__.py
+++ blueskyweb/__init__.py
@@ -1,4 +1,7 @@
 """bluesky.web"""
 
+__version_info__ = (0,1,0)
+__version__ = '.'.join([str(n) for n in __version_info__])
+
 __author__      = "Joel Dubowy"
 __copyright__   = "Copyright 2015, AirFire, PNW, USFS"
