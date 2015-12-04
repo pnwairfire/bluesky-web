0f9b17f5e167ce9a50e79bf441e82f9c3b1aec09
Joel Dubowy <jdubowy@gmail.com>
2015-12-04 14:04:35 -0800

    Update domain APIs to return data from mongodb

--- blueskyweb/api/v1/domain.py
+++ blueskyweb/api/v1/domain.py
@@ -7,91 +7,32 @@ import datetime
 import json
 import tornado.web
 
-## ***
-## *** TODO: REPLACE DUMMY DATA WITH REAL!!!
-## ***
-## *** Will need to add configuration options to web service to point
-## *** to source of data (e.g. url of mongodb containing the data vs.
-## *** root url or path to crawl for data vs. something else...)
-## ***
-
-DUMMY_DOMAIN_DATA = {
-    "NAM84": {
-        "dates": [
-            datetime.date.today().strftime("%Y%m%d"),
-            (datetime.date.today() - datetime.timedelta(1)).strftime("%Y%m%d"),
-            (datetime.date.today() - datetime.timedelta(2)).strftime("%Y%m%d")
-        ],
-        "boundary": {
-            "center_latitude": 37.5,
-            "center_longitude": -95.0,
-            "width_longitude": 70.0,
-            "height_latitude": 30.0,
-            "spacing_longitude": 0.5,  # TODO: is this correct?
-            "spacing_latitude": 0.5  # TODO: is this correct?
-        }
-    },
-    "DRI2km": {
-        "dates": [
-            datetime.date.today().strftime("%Y%m%d"),
-            (datetime.date.today() - datetime.timedelta(1)).strftime("%Y%m%d"),
-            (datetime.date.today() - datetime.timedelta(2)).strftime("%Y%m%d")
-        ],
-        "boundary": {
-            "center_latitude": 37.0,
-            "center_longitude": -119.0,
-            "width_longitude": 13.0,
-            "height_latitude": 11.5,
-            "spacing_longitude": 0.1,
-            "spacing_latitude": 0.1
-        }
-    },
-    "DRI6km": {
-        "dates": [
-            datetime.date.today().strftime("%Y%m%d"),
-            (datetime.date.today() - datetime.timedelta(1)).strftime("%Y%m%d"),
-            (datetime.date.today() - datetime.timedelta(2)).strftime("%Y%m%d")
-        ],
-        "boundary": {
-            "center_latitude": 36.5,
-            "center_longitude": -119.0,
-            "width_longitude": 25.0,
-            "height_latitude": 17.5,
-            "spacing_longitude": 0.5,
-            "spacing_latitude": 0.5
-        }
-    }
-}
+from blueskyweb.lib import domains
 
 class DomainInfo(tornado.web.RequestHandler):
 
     def get(self, domain_id=None):
-        if not domain_id:
-            self.write({
-                "domains": DUMMY_DOMAIN_DATA,
-                "IS_DUMMY_DATA": True
-            })
-        elif domain_id in DUMMY_DOMAIN_DATA:
-            self.write({
-                domain_id: DUMMY_DOMAIN_DATA[domain_id],
-                "IS_DUMMY_DATA": True
-            })
-        else:
-            self.set_status(404, "Domain does not exist")
+        data = domains.DomainDB().find(domain_id=domain_id)
 
+        if domain_id:
+            if not data:
+                self.set_status(404, "Domain does not exist")
+            else:
+                self.write(data)
+        else:
+            self.write({"domains": data})
 
 class DomainAvailableDates(tornado.web.RequestHandler):
 
     def get(self, domain_id=None):
-        if not domain_id:
-            self.write({
-                "dates": {d: data['dates'] for d,data in DUMMY_DOMAIN_DATA.items()},
-                "IS_DUMMY_DATA": True
-            })
-        elif domain_id in DUMMY_DOMAIN_DATA:
-            self.write({
-                "dates": DUMMY_DOMAIN_DATA[domain_id]["dates"],
-                "IS_DUMMY_DATA": True
-            })
+        data = domains.DomainDB().find(domain_id=domain_id)
+
+        if domain_id:
+            if not data:
+                self.set_status(404, "Domain does not exist")
+            else:
+                self.write({"dates":data[domain_id]["dates"]})
         else:
-            self.set_status(404, "Domain does not exist")
+            self.write({
+                "dates": {d: data[d]['dates'] for d in data}
+            })
