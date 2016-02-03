02e7b29692c57ebd41137a9ffb3755c6f029b5e9
Joel Dubowy <jdubowy@gmail.com>
2016-02-02 16:45:02 -0800

    Upgrade bluesky to v0.7.10

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         "pyairfire>=0.9.2",
         "tornado==4.3.0",
-        "bluesky>=0.7.7",
+        "bluesky>=0.7.10",
         "requests>=2.7.0"
     ],
     dependency_links=[
