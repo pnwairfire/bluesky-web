3dcc71041329f2568ba8034c369a9fc545596398
Joel Dubowy <jdubowy@gmail.com>
2017-11-27 13:50:44 -0800

    Continued work on tests for hysplit grid reduction logic

--- test/unit/blueskyweb/lib/test_hysplit.py
+++ test/unit/blueskyweb/lib/test_hysplit.py
@@ -20,15 +20,15 @@ class MockRequestHandler(object):
             return self.get
 
 ARCHIVE_INFO = {
-    "id": "ca-nv_2-km",
-    "title": "CA/NV 2-km",
+    "id": "dummy-archive-2km",
+    "title": "Dummy Archive 2km",
     "domain_id": "DRI2km",
     "grid": {
         "spacing": 2,
         "projection": "LCC",
         "boundary": {
-            "sw": {"lng": -124.3, "lat": 32.5},
-            "ne": {"lng": -113.0, "lat": 41.8}
+            "sw": {"lng": -100.0, "lat": 30.0},
+            "ne": {"lng": -60.0, "lat": 40.0}
         }
     },
     "arl_index_file": "arl12hrindex.csv",
@@ -42,9 +42,9 @@ class TestHysplitConfiguratorConfigureReducedGrid(object):
         input_data = {
             "config": {"dispersion": {}},
             "fire_information": [
-                {"growth": [{"location": {"latitude": 37.909644,
-                    "longitude": -119.7615805}}]},
-                {"growth": [{"location": {"latitude": 32.909644,
+                {"growth": [{"location": {"latitude": 37.0,
+                    "longitude": -89.0}}]},
+                {"growth": [{"location": {"latitude": 32.0,
                     "longitude": -112.0}}]}
             ]
         }
@@ -58,7 +58,7 @@ class TestHysplitConfiguratorConfigureReducedGrid(object):
             "config": {"dispersion": {}},
             "fire_information": [
                 {"growth": [{"location": {
-                    "longitude": -119.7615805}}]}
+                    "longitude": -89.0}}]}
             ]
         }
         hycon = hysplit.HysplitConfigurator(
@@ -67,13 +67,103 @@ class TestHysplitConfiguratorConfigureReducedGrid(object):
             hycon._configure_hysplit_reduced_grid()
 
     def test_not_reduced(self):
+        """This tests the case where the grid is not reduced - it
+        is left as defined for the archive.
+        """
         input_data = {
             "config": {"dispersion": {}},
             "fire_information": [
-                {"growth": [{"location": {"latitude": 37.909644,
-                    "longitude": -119.7615805}}]}
+                {"growth": [{"location": {"latitude": 37.0,
+                    "longitude": -89.0}}]}
             ]
         }
         hycon = hysplit.HysplitConfigurator(
             MockRequestHandler(), input_data, ARCHIVE_INFO)
         hycon._configure_hysplit_reduced_grid()
+        expected_grid = {
+            'boundary': {
+                "sw": {"lng": -100.0, "lat": 30.0},
+                "ne": {"lng": -60.0, "lat": 40.0}
+            },
+            'projection': 'LCC',
+            'spacing': 2.0
+        }
+        assert hycon._hysplit_config['grid'] == expected_grid
+
+    def test_reduced_fire_in_middle(self):
+        """This tests the case where the grid is reduced, and the fire
+        is central enough so that the reduced grid can be centered
+        around it.
+        """
+        input_data = {
+            "config": {"dispersion": {}},
+            "fire_information": [
+                {"growth": [{"location": {"latitude": 37.0,
+                    "longitude": -84.0}}]}
+            ]
+        }
+        hycon = hysplit.HysplitConfigurator(
+            MockRequestHandler(grid_size=0.50),
+            input_data, ARCHIVE_INFO)
+        hycon._configure_hysplit_reduced_grid()
+        expected_grid = {
+            'boundary': {
+                "sw": {"lng": -94.0, "lat": 34.5},
+                "ne": {"lng": -74.0, "lat": 39.5}
+            },
+            'projection': 'LCC',
+            'spacing': 2.0
+        }
+        assert hycon._hysplit_config['grid'] == expected_grid
+
+    def test_reduced_fire_in_middle_left(self):
+        """This tests the case where the grid is reduced, and the fire
+        is central enough in one dimension for the reduced grid to
+        be centered around it, but not in the other.
+        """
+        input_data = {
+            "config": {"dispersion": {}},
+            "fire_information": [
+                {"growth": [{"location": {"latitude": 37.0,
+                    "longitude": -94.0}}]}
+            ]
+        }
+        hycon = hysplit.HysplitConfigurator(
+            MockRequestHandler(grid_size=0.50),
+            input_data, ARCHIVE_INFO)
+        hycon._configure_hysplit_reduced_grid()
+        expected_grid = {
+            'boundary': {
+                "sw": {"lng": -100.0, "lat": 34.5},
+                "ne": {"lng": -80.0, "lat": 39.5}
+            },
+            'projection': 'LCC',
+            'spacing': 2.0
+        }
+        assert hycon._hysplit_config['grid'] == expected_grid
+
+    def test_reduced_fire_in_upper_right(self):
+        """This tests the case where the grid is reduced, and the fire
+        is far enough in the corner of the archive's full grid that
+        it's also toward the corner in the reduced grid.
+        """
+        input_data = {
+            "config": {"dispersion": {}},
+            "fire_information": [
+                {"growth": [{"location": {"latitude": 31.0,
+                    "longitude": -64.0}}]}
+            ]
+        }
+        hycon = hysplit.HysplitConfigurator(
+            MockRequestHandler(grid_size=0.50),
+            input_data, ARCHIVE_INFO)
+        hycon._configure_hysplit_reduced_grid()
+        expected_grid = {
+            'boundary': {
+                "sw": {"lng": -80.0, "lat": 30.0},
+                "ne": {"lng": -60.0, "lat": 35.0}
+            },
+            'projection': 'LCC',
+            'spacing': 2.0
+        }
+        assert hycon._hysplit_config['grid'] == expected_grid
