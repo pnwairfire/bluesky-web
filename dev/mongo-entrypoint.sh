e6863de629a6e12230be943d2672bb4e68518942
Joel Dubowy <jdubowy@gmail.com>
2019-04-18 14:31:07 -0700

    Add missing ssl related flags to mongo command in dev entrypoint script

--- dev/mongo-entrypoint.sh
+++ dev/mongo-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 echo "Creating mongo users..."
-mongo blueskyweb --authenticationDatabase admin --host localhost -u blueskywebadmin -p blueskywebmongopasswordadmin --eval "db.createUser({user: 'blueskyweb', pwd: 'blueskywebmongopassword', roles: [{role: 'readWrite', db: 'blueskyweb'}]});"
+mongo --ssl --sslPEMKeyFile /etc/ssl/bluesky-web-mongod.pem  \
+    --sslAllowInvalidHostnames --sslAllowInvalidCertificates  \
+    blueskyweb --authenticationDatabase admin --host localhost \
+    -u blueskywebadmin -p blueskywebmongopasswordadmin \
+    --eval "db.createUser({user: 'blueskyweb', pwd: 'blueskywebmongopassword', roles: [{role: 'readWrite', db: 'blueskyweb'}]});"
 echo "Mongo users created."
