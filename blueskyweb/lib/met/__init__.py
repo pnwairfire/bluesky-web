5bc25f4c7f348701bb7b7d31c45ce83005d2805f
Joel Dubowy <jdubowy@gmail.com>
2019-08-26 12:00:40 -0700

    Move future common API code from blueskyweb.api.v1 to blueskyweb.lib.api

--- blueskyweb/lib/met/__init__.py
+++ blueskyweb/lib/met/__init__.py
@@ -1,6 +1,6 @@
-from . import db, api
+from . import db
 
-# The above imports support calling met.db and met.api after importing the
-# met subpackage, but we don't want 'db' and 'api' to polute the global
+# The above import supports calling met.db after importing the met
+# subpackage, but we don't want 'db' to pollute the global
 # namespace if * is imported from blueskyweb.lib.met
 __all__ = []
