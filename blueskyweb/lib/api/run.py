166e7b71322fde1a9e9e4c8dedbb2bdd36ca7bd6
Joel Dubowy <jdubowy@gmail.com>
2019-08-28 15:18:40 -0700

    Separate run output processing code from request handling code
    to enable running the code outside the context of an http request

--- blueskyweb/lib/api/run.py
+++ blueskyweb/lib/api/run.py
@@ -19,6 +19,7 @@ from blueskymongo.client import RunStatuses
 from blueskyworker.tasks import process_runtime
 from blueskyweb.lib import met
 from blueskyweb.lib.runs.execute import BlueSkyRunExecuter
+from blueskyweb.lib.runs.output import BlueSkyRunOutput
 from . import RequestHandlerBase
 
 
@@ -30,34 +31,8 @@ except:
     # resort to loading all output as if from remote server
     pass
 
-##
-## Utilities for working with remote output
-##
 
-class remote_open(object):
-    """Context manager that clones opens remote file and closes it on exit
-    """
 
-    def __init__(self, url):
-        self.url = url
-
-    def __enter__(self):
-        self.f = urllib.request.urlopen(self.url)
-        return self.f
-
-    def __exit__(self, type, value, traceback):
-        self.f.close()
-
-def remote_exists(url):
-    return requests.head(url).status_code != 404
-
-def get_output_server_info(run_id):
-    output_server_info = {} # TODO: Get info from mongodb
-    return output_server_info
-
-def get_output_url(run_id):
-
-    return "{}{}".format(get_output_root_url(run_id), url_root_dir)
 
 # PORT_IN_HOSTNAME_MATCHER = re.compile(':\d+')
 # def is_same_host(web_request_host):
@@ -257,135 +232,4 @@ class RunOutput(RequestHandlerBase):
             self._raise_error(404, "Run output doesn't exist")
 
         else:
-            if 'dispersion' in run['modules']:
-                #if output['config']['dispersion'].get('model') != 'vsmoke'):
-                self._get_dispersion(run)
-            elif 'plumerising' in run['modules']:
-                self._get_plumerise(run)
-            else:
-                # TODO: is returning raw input not ok?
-                output = self._load_output(run)
-                self.write(output)
-
-    ##
-    ## Plumerise
-    ##
-
-    def _slice(self, info_dict, whitelist):
-        # Need to create list of keys in order to pop within iteration
-        for k in list(info_dict.keys()):
-            if k not in whitelist:
-                info_dict.pop(k)
-
-    def _get_plumerise(self, run):
-        version_info = run.get('version_info') or {}
-        run_info = run if 'fires' in run else self._load_output(run)
-        fires = run_info['fires']
-        runtime_info = process_runtime(run_info.get('runtime'))
-
-        self.write(dict(run_id=run['run_id'],
-            fires=fires,
-            runtime=runtime_info,
-            version_info=version_info))
-
-    ##
-    ## Dispersion
-    ##
-
-    def _get_dispersion(self, run):
-        r = {
-            "root_url": run['output_url'],
-            "version_info": run.get('version_info') or {}
-        }
-        run_info = run if 'export' in run else self._load_output(run)
-
-        # TODO: refine what runtime info is returned
-        r['runtime'] = process_runtime(run_info.get('runtime'))
-
-        export_info = run_info['export']
-
-        vis_info = export_info['localsave'].get('visualization')
-        if vis_info:
-            # images
-            self._parse_images(r, vis_info)
-
-            # kmzs
-            self._parse_kmzs_info(r, vis_info)
-
-        disp_info = export_info['localsave'].get('dispersion')
-        if disp_info:
-            r.update(**{
-                k: '{}/{}'.format(disp_info['sub_directory'], disp_info[k.lower()])
-                for k in ('netCDF', 'netCDFs') if k.lower() in disp_info})
-
-            # kmzs (vsmoke dispersion produces kmzs)
-            self._parse_kmzs_info(r, disp_info)
-
-        # TODO: list fire_*.csv if specified in output
-
-        self.write(r)
-
-    def _parse_kmzs_info(self, r, section_info):
-        kmz_info = section_info.get('kmzs', {})
-        if kmz_info:
-            r['kmzs'] = {k: '{}/{}'.format(section_info['sub_directory'], v)
-                for k, v in list(kmz_info.items()) if k in ('fire', 'smoke')}
-
-    def _parse_images(self, r, vis_info):
-        r["images"] = vis_info.get('images')
-        def _parse(r):
-            if "directory" in d:
-                d["directory"] = os.path.join(
-                    vis_info['sub_directory'], d["directory"])
-            else:
-                for e in d:
-                    _parse(e)
-
-
-    ##
-    ## Common methods
-    ##
-
-    def _load_output(self, run):
-        # TODO: Maybe first try local no matter what, since is_same_host might
-        #   give false negative and checking local shouldn't give false postive
-        #   (only do this if is_same_host returns false negative in production)
-        if is_same_host(run):
-            tornado.log.gen_log.debug('Loading local output')
-            return self._get(run['output_dir'], os.path.exists, open)
-        else:
-            tornado.log.gen_log.debug('Loading remote output')
-            return self._get(run['output_url'], remote_exists, remote_open)
-
-    def _get(self, output_location, exists_func, open_func):
-        """Gets information about the outpu output, which may be in local dir
-        or on remote host
-
-        args:
-         - output_location -- local pathname or url
-         - exists_func -- function to check existence of dir or file
-            (local or via http)
-         - open_func -- function to open output json file (local or via http)
-        """
-        tornado.log.gen_log.debug('Looking for output in %s', output_location)
-        if not exists_func(output_location):
-            msg = "Output location doesn't exist: {}".format(output_location)
-            self._raise_error(404, msg)
-
-        # use join instead of os.path.join in case output_location is a remote url
-        output_json_file = '/'.join([output_location.rstrip('/'), 'output.json'])
-        if not exists_func(output_json_file):
-            msg = "Output file doesn't exist: {}".format(output_json_file)
-            self._raise_error(404, msg)
-
-        with open_func(output_json_file) as f:
-            try:
-                j = f.read()
-                if hasattr(j, 'decode'):
-                    j = j.decode()
-                return json.loads(j)
-                # TODO: set fields here, using , etc.
-            except:
-                msg = "Failed to open output file: {}".format(output_json_file)
-                self._raise_error(500, msg)
-
+            BlueSkyRunOutput(run, self._raise_error, self.write).process()
