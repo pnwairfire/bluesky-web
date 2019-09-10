de33f4bb5f8e2b7fa2ec2dbebd45732c3d501453
Joel Dubowy <jdubowy@gmail.com>
2019-09-10 09:40:03 -0700

    Include bluesky version in ping API reponse

--- blueskyweb/api/ping.py
+++ blueskyweb/api/ping.py
@@ -4,6 +4,8 @@ __author__      = "Joel Dubowy"
 __copyright__   = "Copyright 2015, AirFire, PNW, USFS"
 
 import tornado.web
+from bluesky import __version__
+
 
 __all__ = [
     'Ping'
@@ -13,4 +15,4 @@ class Ping(tornado.web.RequestHandler):
 
     def get(self):
         # TODO: return anything else?
-        self.write({"msg": "pong"})
+        self.write({"msg": "pong", "blueskyVersion": __version__})
