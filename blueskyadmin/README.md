4a1ea61aff97aff5e085f9947f650b87b79d30a1
Joel Dubowy <jdubowy@gmail.com>
2023-02-14 12:39:59 -0800

    Add page for viewing runs in the queue (either for later or for next available worker) as well as runs currently being processed.

--- blueskyadmin/README.md
+++ blueskyadmin/README.md
@@ -1,1 +1,16 @@
 # BlueSky-Web Admin
+
+## Dev Setup
+
+create file `.env` defining `PUBLIC_API_URL`, e.g.
+
+    PUBLIC_API_URL=http://web1/bluesky-web/api/v4.2`
+
+## Running outside of docker
+
+    cd pnwairfire-bluesky-web/blueskyadmin
+    ./run-no-docker
+
+## Running in docker
+
+See [README](../README.md) in repo root.
---
+++ blueskyadmin/src/routes/[basePath=validbasepath]/admin/queue/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+    import { Container, Nav, NavItem, NavLink, Table } from 'sveltestrap';
+
+    /** @type {import('./$types').PageData} */
+    export let data;
+
+</script>
+
+<Container fluid="true">
+    {#if data.error}
+        {data.error}
+    {:else}
+        {#each Object.keys(data.queueInfo) as category, i}
+            <b>{category}</b>
+            {#each Object.keys(data.queueInfo[category]) as pool, j}
+                <div class="m-3">
+                    <b>{pool}</b>
+                {#if data.queueInfo[category][pool].length === 0}
+                    (None)
+                {:else}
+                    <div class="m-3">
+                        <Table bordered hover striped size="sm" responsive>
+                          <thead>
+                            <tr>
+                              <th>Run Id</th>
+                              <th>Modules</th>
+                              <th>API version</th>
+                              <th>Priority</th>
+                              <th>Scheduled For</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {#each data.queueInfo[category][pool] as run}
+                                <tr>
+                                  <th>{run.run_id}</th>
+                                  <th>{run.modules.join(', ')}</th>
+                                  <th>{run.api_version}</th>
+                                  <th>{run.priority}</th>
+                                  <th>{run.schedule_for}</th>
+                                </tr>
+                            {/each}
+                          </tbody>
+                        </Table>
+                    </div>
+                {/if}
+                </div>
+            {/each}
+        {/each}
+    {/if}
+</Container>
