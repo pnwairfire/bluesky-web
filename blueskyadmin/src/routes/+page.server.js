1e29a8bfebb0dff2e432bfae48cfccf9d45faf7a
Joel Dubowy <jdubowy@gmail.com>
2023-02-15 13:30:08 -0800

    Clean up cruft related to redirects

--- blueskyadmin/src/routes/+page.server.js
+++ blueskyadmin/src/routes/+page.server.js
@@ -2,7 +2,5 @@ import { redirect } from '@sveltejs/kit';
  
 /** @type {import('./$types').LayoutServerLoad} */
 export function load({ locals, params }) {
-  if (!locals.user) {
-    throw redirect(307, `/bluesky-web/admin/runs/enqueued`);
-  }
+  throw redirect(307, `/bluesky-web/admin/runs/enqueued`);
 }
