66f40ca60032ff27165b4faf56431c3b24794734
Joel Dubowy <jdubowy@gmail.com>
2023-02-16 13:53:42 -0800

    Tweak header navbar links text

--- blueskyadmin/src/routes/Header.svelte
+++ blueskyadmin/src/routes/Header.svelte
@@ -19,10 +19,10 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" href={`/${basePath}/admin/queue`}>Queue</a>
+            <a class="nav-link" href={`/${basePath}/admin/queue`}>Worker Queues</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href={`/${basePath}/admin/runs`}>Runs</a>
+            <a class="nav-link" href={`/${basePath}/admin/runs`}>Runs Database</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href={`/${basePath}/admin/about`}>About</a>
