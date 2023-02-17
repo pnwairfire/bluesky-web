6a204cfa3b636c50200b4708766690d50e29aecc
Joel Dubowy <jdubowy@gmail.com>
2023-02-17 11:28:03 -0800

    Update queue API endpoint to correctly handle all three categories of tasks - active, in queue, and scheduled for later

--- blueskyweb/api/queue.py
+++ blueskyweb/api/queue.py
@@ -16,24 +16,139 @@ class QueueInfo(RequestHandlerBase):
 
         self.write({
             #"registered": i.registered(),
-            "in_queue": prune_worker_set(i.reserved()),
-            "executing": prune_worker_set(i.active()),
-            "scheduled": prune_worker_set(i.scheduled()),
+            "in_queue": prune_worker_set(i.reserved(), prune_enqueud_or_active),
+            "executing": prune_worker_set(i.active(), prune_enqueud_or_active),
+            "scheduled": prune_worker_set(i.scheduled(), prune_scheduled),
         })
 
 
-def prune_worker_set(r):
-    return {w: [prune_task(t) for t in r[w]] for w in r}
+def prune_worker_set(r, prune_func):
+    return {w: [prune_func(t) for t in r[w]] for w in r}
 
 
-def prune_task(task):
+def prune_scheduled(task):
+    """Active tasks have the following structure.
+
+        ***Note that 'args' and 'kwargs' are strings, but are
+        ***expanded here with newlines and spaces fore readability
+
+        {
+            'eta': '2023-02-17T19:04:24+00:00',
+            'priority': 6,
+            'request': {
+                'id': 'f4a75267-b91e-4ef3-bc0e-1ee3af128463',
+                'name': 'blueskyworker.tasks.run_bluesky',
+                'args': "(
+                    {
+                        'bluesky_version': '4.3.71',
+                        'fires': [{...}, {...}],
+                        'run_id': 'abc123',
+                        'summary': {'fuelbeds': [...]},
+                        'modules': ['ecoregion', 'consumption', 'emissions'],
+                        'config': {'fuelbeds': {...}, 'emissions': {...}}
+                    },
+                    '4.2'
+                )",
+                'kwargs': "{
+                    'port': '80',
+                    'mongodb_url': 'mongodb://blueskyweb:blueskywebmongopassword@mongo/blueskyweb',
+                    'rabbitmq_url': 'amqps://blueskyweb:blueskywebrabbitpassword@rabbit:5671',
+                    'log_file': '/var/log/blueskyweb/bluesky-web.log',
+                    'output_url_scheme': 'http',
+                    'output_url_port': '8886',
+                    'output_url_path_prefix': 'bluesky-web-output',
+                    'bluesky_log_level': 'DEBUG',
+                    'path_prefix': '/bluesky-web',
+                    'debug': True,
+                    'output_root_dir': '/data/bluesky/output/',
+                    'log_level': 10,
+                    'autoreload': True,
+                    'compiled_template_cache': False,
+                    'static_hash_cache': False,
+                    'serve_traceback': True
+                }",
+                'type': 'blueskyworker.tasks.run_bluesky',
+                'hostname': 'celery@bluesky-web-worker',
+                'time_start': None,
+                'acknowledged': False,
+                'delivery_info': {
+                    'exchange': '',
+                    'routing_key': 'no-met',
+                    'priority': None,
+                    'redelivered': False
+                },
+                'worker_pid': None
+            }
+        }
+
+    """
+    tornado.log.gen_log.debug(f"***TASK***: {task}")
     request_args = eval(task["request"]["args"])
     t = {
         "priority": task["priority"],
+        "id": task["request"]["id"],
+        "type": task["request"]["type"],
         "api_version": request_args[1],
         "run_id": request_args[0]['run_id'],
         "modules": request_args[0]['modules']
     }
     if task.get('eta'):
         t['schedule_for'] = task['eta']
+    return t
+
+def prune_enqueud_or_active(task):
+    """Active tasks have the following structure.
+
+        ***Note that 'args' and 'kwargs' are strings, but are
+        ***expanded here with newlines and spaces fore readability
+
+        {
+            'id': '2743d663-e4b5-459e-82e1-eca365f0309d',
+            'name': 'blueskyworker.tasks.run_bluesky',
+            'args': "(
+                {
+                    'config': {'emissions': {...},'dispersion': {...},'visualization': {...},'export': {...},'fuelbeds': {...},'consumption': {...},'plumerise': {...},'findmetdata': {...},'extrafiles': {...}},
+                    'fires': [{...}, {...}],
+                    'run_id': 'test-asynch-request-dispersion-20230217T180122',
+                    'modules': ['fuelbeds', 'ecoregion', 'consumption', 'emissions', 'timeprofile', 'plumerise', 'findmetdata', 'extrafiles', 'dispersion', 'visualization', 'export']
+                },
+                '4.2'
+            )",
+            'kwargs': "{
+                'port': '80', 'mongodb_url': 'mongodb://blueskyweb:blueskywebmongopassword@mongo/blueskyweb',
+                'rabbitmq_url': 'amqps://blueskyweb:blueskywebrabbitpassword@rabbit:5671', 'log_file': '/var/log/blueskyweb/bluesky-web.log',
+                'output_url_scheme': 'http',
+                'output_url_port': '8886',
+                'output_url_path_prefix': 'bluesky-web-output',
+                'bluesky_log_level': 'DEBUG',
+                'path_prefix': '/bluesky-web',
+                'debug': True,
+                'output_root_dir': '/data/bluesky/output/',
+                'log_level': 10,
+                'autoreload': True,
+                'compiled_template_cache': False,
+                'static_hash_cache': False,
+                'serve_traceback': True
+            }",
+            'type': 'blueskyworker.tasks.run_bluesky',
+            'hostname': 'celery@bluesky-web-worker',
+            'time_start': 3263068.543072896,
+            'acknowledged': True,
+            'delivery_info': {
+                'exchange': '',
+                'routing_key': 'pacific_northwest_4-km',
+                'priority': None,
+                'redelivered': False
+            },
+            'worker_pid': 14
+        }
+    """
+    request_args = eval(task["args"])
+    t = {
+        "id": task["id"],
+        "type": task["type"],
+        "api_version": request_args[1],
+        "run_id": request_args[0]['run_id'],
+        "modules": request_args[0]['modules']
+    }
     return t
