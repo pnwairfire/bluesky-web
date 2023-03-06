64a5560c06f7db25038401ae17c332c10c1ad708
Joel Dubowy <jdubowy@gmail.com>
2023-03-06 14:23:10 -0800

    Remove hardcoded base path

--- blueskyadmin/src/routes/+page.server.js
+++ blueskyadmin/src/routes/+page.server.js
@@ -1,6 +1,7 @@
 import { redirect } from '@sveltejs/kit';
+import { base } from '$app/paths';
  
 /** @type {import('./$types').LayoutServerLoad} */
 export function load({ locals, params }) {
-  throw redirect(307, `/bluesky-web/admin/runs`);
+  throw redirect(307, `${base}/runs`);
 }
