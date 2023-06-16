d622bd467f30029d0e308b26f83df81a23c9d3d5
Joel Dubowy <jdubowy@gmail.com>
2023-06-15 17:10:24 -0700

    Support limiting number of output files to process and use more flexible timestmamp parsing

--- dev/scripts/count-dispersion-runs.py
+++ dev/scripts/count-dispersion-runs.py
@@ -15,23 +15,42 @@ def parse_args():
     parser.add_argument('-d', '--output-root-dir', required=True,
         help="Directoring containing output/")
     parser.add_argument('-p', '--glob-pattern',
-        default="**/output.json", help="Directoring containing output/")
+        default="**/output.json", help="glob pattern")
+    parser.add_argument('-l', '--limit', type=int,
+        help="Number of output to process, starting with most recent")
 
     return parser.parse_args()
 
-def find_output_files(root_dir, pattern):
+def find_output_files(root_dir, pattern, limit):
     print("Finding files like {} in {}".format(pattern, root_dir))
-    return glob.glob(os.path.join(root_dir, pattern))
+    files = glob.glob(os.path.join(root_dir, pattern))
+    files.sort(key=os.path.getmtime)
+    files.reverse()
+    if limit:
+        files = files[0:limit]
+    return files
+
+PATTERNS = ("%Y-%m-%dT%H:%M:%S.%fZ", '%Y-%m-%dT%H:%M:%SZ', '%Y-%m-%dT%H:%M:%S')
+def parse_ts(ts):
+    for p in PATTERNS:
+        try:
+            return datetime.datetime.strptime(ts, p)
+        except:
+            # move on to the next
+            pass
+
+    # none succeeded; error
+    raise RuntimeError("Couldn't parse ts {}".format(ts))
 
 def bucket(output_file):
     try:
         with open(output_file) as f:
             data = json.loads(f.read())
-        run_ts_obj = datetime.datetime.strptime(data['runtime']['start'], "%Y-%m-%dT%H:%M:%S.%fZ")
+        run_ts_obj = parse_ts(data['runtime']['start'])
         disp_start = data.get('dispersion',{}).get('output', {}).get('start_time')
         if not disp_start:
             return 'na'
-        disp_start_obj = datetime.datetime.strptime(disp_start, '%Y-%m-%dT%H:%M:%S')
+        disp_start_obj = parse_ts(disp_start)
         if run_ts_obj - disp_start_obj > datetime.timedelta(days=6):
             return 'retro'
         else:
@@ -43,12 +62,13 @@ def bucket(output_file):
 
 if __name__ == "__main__":
     args = parse_args()
-    files = find_output_files(args.output_root_dir, args.glob_pattern)
+    files = find_output_files(args.output_root_dir, args.glob_pattern, args.limit)
     print("Processing {} output files".format(len(files)))
     counts = {}
     for i, f in enumerate(files):
         if i % 10 == 0:
             sys.stdout.write('.')
+            sys.stdout.flush()
         b = bucket(f)
         counts[b] = counts.get(b, 0) + 1
     sys.stdout.write('\n')
